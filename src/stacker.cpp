1998c7571741edbb65422100ba49aa2c8598f25c
Andrew Olson <olson@cshl.edu>
2012-05-18 11:37:40 -0400

    added comments before each function and a fillResult() stub

--- src/stacker.cpp
+++ src/stacker.cpp
@@ -37,7 +37,8 @@ ibis::qExpr* senseExpr = senseWhere.getExpr();
 
 
 // printout the usage string
-static void usage(const char* name) {
+static void usage(const char* name)
+{
   std::cout << "usage:\n" << name << std::endl
 	    << "[-d1 directory containing A dataset] " << std::endl
 	    << "[-c1 columns from A]" << std::endl
@@ -55,97 +56,97 @@ static void usage(const char* name) {
 		<< "[-sw define -l and -r based on strand]" << std::endl
 		<< "[-b number of bins to use for normalization after stacking. default: don't normalize]" << std::endl
 		<< "[-p parallelize using threads]" << std::endl;
-} // usage
+}
 
 // function to parse the command line arguments
-void parse_args(int argc, char** argv) {
-  
-  for (int i=1; i<argc; ++i) {
-    if (*argv[i] == '-') { // normal arguments starting with -
-      switch (argv[i][1]) {
-	      default:
-	      case 'h':
-	      case 'H':
-			usage(*argv);
-			exit(0);
-		  case 'p':
-			  parallelize=1;
-			  break;
-  		  case 'b':
-  			  if(i+1 < argc) {
-  				  ++i;
-  				  bins = atoi(argv[i]);
-  			  }
-  			  break;
-		  case 'l':
-			  if(i+1 < argc) {
-				  ++i;
-				  left = atoi(argv[i]);
-			  }
-			  break;
-		  case 'r':
-			  if(i+1 < argc) {
-				  ++i;
-				  right = atoi(argv[i]);
-			  }
-			  break;
-	      case 'd':
-	      case 'D':
-			if (i+1 < argc) {
-			  if (argv[i][2] == '1')
-				  Afrom = argv[++i];
-			  else
-				  Bfrom = argv[++i];
-			}
-			break;
-  	      case 'c':
-  			if (i+1 < argc) {
-  				if (argv[i][2] == '1')
-					Asel = argv[++i];
-				else
-					Bsel = argv[++i];
+void parse_args(int argc, char** argv)
+{
+	for (int i=1; i<argc; ++i) {
+	    if (*argv[i] == '-') { // normal arguments starting with -
+	      switch (argv[i][1]) {
+		      default:
+		      case 'h':
+		      case 'H':
+				usage(*argv);
+				exit(0);
+			  case 'p':
+				  parallelize=1;
+				  break;
+	  		  case 'b':
+	  			  if(i+1 < argc) {
+	  				  ++i;
+	  				  bins = atoi(argv[i]);
+	  			  }
+	  			  break;
+			  case 'l':
+				  if(i+1 < argc) {
+					  ++i;
+					  left = atoi(argv[i]);
+				  }
+				  break;
+			  case 'r':
+				  if(i+1 < argc) {
+					  ++i;
+					  right = atoi(argv[i]);
+				  }
+				  break;
+		      case 'd':
+		      case 'D':
+				if (i+1 < argc) {
+				  if (argv[i][2] == '1')
+					  Afrom = argv[++i];
+				  else
+					  Bfrom = argv[++i];
+				}
 				break;
-			}
-			break;
-	      case 'e':
-			if (i+1 < argc) {
-				if (argv[i][2] == '1')
-					Aend = argv[++i];
-				else
-					Bend = argv[++i];
-  				break;
-  			}
-  			break;
-	      case 's':
-			switch (argv[i][2]) {
-				case '1':
-					Astart = argv[++i];
+	  	      case 'c':
+	  			if (i+1 < argc) {
+	  				if (argv[i][2] == '1')
+						Asel = argv[++i];
+					else
+						Bsel = argv[++i];
 					break;
-				case '2':
-					Bstart = argv[++i];
-					break;
-				case 'm':
-					same_strand = 1;
-					break;
-				case 'w':
-					stranded_windows = 1;
-					break;
-			}
-			break;
-	      case 'w':
-	      case 'W':
-			if (i+1 < argc) {
-			  if (argv[i][2] == '1')
-				  Aqcnd = argv[++i];
-			  else
-				  Bqcnd = argv[++i];
-			}
-			break;
-      } // switch (argv[i][1])
-    } // normal arguments
-  } // for (inti=1; ...)
+				}
+				break;
+		      case 'e':
+				if (i+1 < argc) {
+					if (argv[i][2] == '1')
+						Aend = argv[++i];
+					else
+						Bend = argv[++i];
+	  				break;
+	  			}
+	  			break;
+		      case 's':
+				switch (argv[i][2]) {
+					case '1':
+						Astart = argv[++i];
+						break;
+					case '2':
+						Bstart = argv[++i];
+						break;
+					case 'm':
+						same_strand = 1;
+						break;
+					case 'w':
+						stranded_windows = 1;
+						break;
+				}
+				break;
+		      case 'w':
+		      case 'W':
+				if (i+1 < argc) {
+				  if (argv[i][2] == '1')
+					  Aqcnd = argv[++i];
+				  else
+					  Bqcnd = argv[++i];
+				}
+				break;
+	      } // switch (argv[i][1])
+	    } // normal arguments
+	} // for (inti=1; ...)
 
-  std::cerr << *argv << " -d1 " << Afrom << " -d2 " << Bfrom
+	std::cerr << *argv << " -d1 " << Afrom << " -d2 " << Bfrom
 	  << " -s1 " << Astart << " -s2 " << Bstart
 	  << " -e1 " << Aend << " -e2 " << Bend
 	  << " -w1 " << Aqcnd << " -w2 " << Bqcnd
@@ -154,9 +155,27 @@ void parse_args(int argc, char** argv) {
 	  << " -b " << bins << " -p " << parallelize
 	  << " -sm " << same_strand << " -sw " << stranded_windows
 	  << std::endl;
-} // parse_args
+}
 
-void Stacker(const ibis::part* Apart, const ibis::part* Bpart, ibis::bitvector Amask, ibis::bitvector Bmask, int before, int after) {
+// Build a table to represent the interval-join of two partitions
+// using two bitvector masks and two arrays of offsets into the matching rows
+// New columns for relative start and end are included (possibly binned)
+// In the end the table needs to be sorted by these columns
+// When Bstart == Bend you don't add an end column
+ibis::table* fillResult(const ibis::part* Apart, const ibis::part* Bpart,
+	ibis::bitvector Amatch, ibis::bitvector Bmatch,
+	std::vector<uint32_t>& Aidx, std::vector<uint32_t>& Bidx)
+{
+	
+}
+
+// fetch intervals from the partitions to be joined
+// identify features that overlap
+// set bits in Amatch and Bmatch so we can fetch only elements that overlap
+// fill two arrays of indexes that say what rows to take from selected columns of A and B
+void Stacker(const ibis::part* Apart, const ibis::part* Bpart,
+	ibis::bitvector Amask, ibis::bitvector Bmask, int before, int after)
+{
 	if (Amask.cnt() == 0 || Bmask.cnt() == 0)
 		return;
 
@@ -171,10 +190,6 @@ void Stacker(const ibis::part* Apart, const ibis::part* Bpart, ibis::bitvector A
 	ibis::array_t<uint32_t> *Aend_val = Aend_col->selectUInts(Amask);
 	ibis::array_t<uint32_t> *Bstart_val = Bstart_col->selectUInts(Bmask);
 	ibis::array_t<uint32_t> *Bend_val = Bend_col->selectUInts(Bmask);
-
-	// iterate over the intervals and identify features that overlap
-	// set bits in Amatch and Bmatch so we can fetch only elements that overlap
-	// also fill two arrays of indexes that say what rows to take from selected columns of A and B
 	
 	ibis::bitvector Amatch;
 	ibis::bitvector Bmatch;
@@ -182,7 +197,6 @@ void Stacker(const ibis::part* Apart, const ibis::part* Bpart, ibis::bitvector A
 	const uint32_t nB = Bmask.cnt();
 	std::vector<uint32_t> Aidx;
 	std::vector<uint32_t> Bidx;
-	std::cerr << "stacker() nA: " << nA << " nB: " << nB << std::endl;
 	uint32_t iA=0;
 	uint32_t iB=0;
 	uint32_t mA=0;
@@ -224,10 +238,19 @@ void Stacker(const ibis::part* Apart, const ibis::part* Bpart, ibis::bitvector A
 	std::cerr << "after Amatch.cnt() = " << Amatch.cnt() << std::endl;
 	std::cerr << "after Bmatch.cnt() = " << Bmatch.cnt() << std::endl;
 
+	if (Amatch.cnt() == 0 || Bmatch.cnt() == 0)
+		return;
+
+	ibis::table *jtable = fillResult(Apart,Bpart,Amatch,Bmatch,Aidx,Bidx);
+
 }
 
-void getHits(const ibis::part* part, ibis::bitvector* mask,
-	 const ibis::qExpr* cond, const char* colName) {
+// apply the user supplied query expression to the given part
+// populate a bitvector mask that corresponds to the matching rows
+// return the number of rows that match
+int countHits(const ibis::part* part, ibis::bitvector* mask,
+	 const ibis::qExpr* cond, const char* colName)
+{
 	if (cond != 0) {
 		ibis::countQuery que(part);
 		int ierr = que.setWhereClause(cond);
@@ -238,9 +261,13 @@ void getHits(const ibis::part* part, ibis::bitvector* mask,
 		ibis::column *c = part->getColumn(colName);
 		c->getNullMask(*mask);
 	}
+	return mask->cnt();
 }
 
-void splitByStrand(const ibis::part* part, ibis::bitvector* mask, ibis::bitvector* plus, ibis::bitvector* minus) {
+// populate bitvectors for hits to the sense and antisense strands
+// Behold the beauty of WAH bitwise operations!
+void splitByStrand(const ibis::part* part, ibis::bitvector* mask, ibis::bitvector* plus, ibis::bitvector* minus)
+{
 	ibis::countQuery que(part);
 	int ierr = que.setWhereClause(senseExpr);
 	ierr = que.evaluate();
@@ -251,19 +278,22 @@ void splitByStrand(const ibis::part* part, ibis::bitvector* mask, ibis::bitvecto
 	*minus &= *mask;
 }
 
-void setupStacker(const ibis::part* Apart, const ibis::part* Bpart) {
-	// do some checks
+// intersecting genomic intervals are always on the same chromsome
+// so we operate on partitions with the same FBchr
+// other command line options control how intervals are selected
+// so we have to handle special cases related to strand
+void setupStacker(const ibis::part* Apart, const ibis::part* Bpart)
+{
+	// TODO: do some checks
 	// count hits to A and B on this chr
 	// make masks based on Aqcnd and Bqcnd
     ibis::bitvector Amask;
-	getHits(Apart, &Amask, Acond, Astart);
-	
-	if (Amask.cnt() == 0) return;
+	int Arows = countHits(Apart, &Amask, Acond, Astart);	
+	if (Arows == 0) return;
 	
     ibis::bitvector Bmask;
-	getHits(Bpart, &Bmask, Bcond, Bstart);
-
-	if (Bmask.cnt() == 0) return;
+	int Brows = countHits(Bpart, &Bmask, Bcond, Bstart);
+	if (Brows == 0) return;
 	
 	if(same_strand == 1 && (Anaty.count("strand") > 0) && (Bnaty.count("strand") > 0)) {
 		// split into subproblems for each strand
@@ -300,7 +330,8 @@ void setupStacker(const ibis::part* Apart, const ibis::part* Bpart) {
 	}
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     parse_args(argc, argv);
 
 	// parse qExpr once so it can be reused in each partition
@@ -344,7 +375,7 @@ int main(int argc, char** argv) {
 		g.join_all();
 
 	// concatenate each part into one table?
-	// orderby()
+	// orderby()?
 
 	// write the table out - user supplied outdir
 	// create one subdirectory partition with FBchr = stacked
@@ -352,4 +383,4 @@ int main(int argc, char** argv) {
 	delete A;
 	delete B;
     return 0;
-} // main
+}

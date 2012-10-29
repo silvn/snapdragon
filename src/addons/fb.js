c5cd62d54f1c5bc65a902e0cbaebc6f851efac23
Andrew Olson <olson@cshl.edu>
2012-10-29 09:17:47 -0400

    fixed qExpr() setup

--- src/addons/fb.js
+++ src/addons/fb.js
@@ -1,7 +1,13 @@
 var fb = require('./build/Release/fb');
 
 console.log(fb.SQL({
+	select: "GO_term,count(*) as tally",
+	from: "/Users/olson/src/garden/iris/examples/fastbit/gene2GO",
+	where: "49000 < gene_id < 50000",
+	orderby: "tally desc"}));
+
+console.log(fb.SQL({
 	select: "GO_branch,count(*) as tally",
 	from: "/Users/olson/src/garden/iris/examples/fastbit/gene2GO",
-	where: "gene_id < 50000",
+	where: "1=1",
 	orderby: "tally desc"}));

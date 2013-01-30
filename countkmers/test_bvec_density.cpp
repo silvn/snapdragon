8912e5a5046041caf57b79e42254affac7cd86c7
Shiran Pasternak <shiranpasternak@gmail.com>
2013-01-29 22:40:40 -0500

    Isolated failed cases, now on length 4. Iterating over test cases.

--- countkmers/test_bvec_density.cpp
+++ countkmers/test_bvec_density.cpp
@@ -1,12 +1,24 @@
 #include "bvec32.h"
 #include <algorithm>
 
-#define DEBUG false
+#define TERM_RESET "\e[m"
+#define TERM_BOLD  "\e[1m"
+#define TERM_RED   "\e[31m"
+#define TERM_GREEN "\e[32m"
+
+#define TEST_VEC_LENGTH 4
+const uint32_t TEST_CASES[][TEST_VEC_LENGTH] = {
+    { 161143029,  1089249216, 1093915745, 1157787156 },
+    { 3085261341, 3247604874, 3456053010, 3773962658 },
+    { 13431029,   377786868,  1463271703, 1501810865 },
+    { 1819233732, 2235579467, 2625766209, 4061912144 }
+};
 
 void print_binary(vector<uint32_t>& v) {
     for (int i = 0; i < v.size(); i++) {
         for (int b = 0; b < 32; b++)
             printf("%d", v[i] & (2 << b) ? 1 : 0);
+        printf(" ");
     }
     printf("\n");
 }
@@ -19,13 +31,40 @@ void xor_vectors(const vector<uint32_t>& a, const vector<uint32_t>& b,
 }
 
 void debug_binary(const char* head, vector<uint32_t>& v) {
-    if (!DEBUG) return;
     printf("%-20s ", head);
     print_binary(v);
 }
 
+void run_test_cases() {
+    int num_cases = sizeof(TEST_CASES) / (TEST_VEC_LENGTH * sizeof(uint32_t));
+    for (int i = 0; i < num_cases; i++) {
+        vector<uint32_t> v;
+        for (int j = 0; j < TEST_VEC_LENGTH; j++)
+            v.push_back(TEST_CASES[i][j]);
+        bvec *bv = new bvec(v);
+        bv->compress();
+        bv->decompress();
+        vector<uint32_t> uncomp = bv->get_words();
+        bool result = v == uncomp;
+        printf(""TERM_BOLD"Test %d"TERM_RESET": %s\n", i, result
+            ? ""TERM_GREEN"Pass"TERM_RESET"" : ""TERM_RED"Fail"TERM_RESET"");
+        if (!result) {
+            vector<uint32_t> va;
+            printf("%-20s ", "Original");
+            for (int j = 0; j < v.size(); j++) printf("%32u ", v[j]);
+            cout << endl;
+            xor_vectors(v, uncomp, va);
+            debug_binary("Original (bin)", v);
+            debug_binary("Uncompressed", uncomp);
+            debug_binary("XOR", va);
+        }
+    }
+}
+
 using namespace std;
 int main(int argc, char *argv[]) {
+    run_test_cases();
+    return 0;
 	// simulate random bitvectors with up to 1 billion points
 	// 16mers - up to 2^32 - 1
 	// 32mers - up to 2^64 - 1

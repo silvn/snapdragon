6e6fb628b53987076713083ec55fe3be150d904b
Andrew Olson <olson@cshl.edu>
2013-02-06 02:04:25 -0500

    mostly working

--- preprocessing/split_nt.cpp
+++ preprocessing/split_nt.cpp
@@ -6,9 +6,9 @@
 #include "kseq.h"
 #include <ctime>
 #include <iostream>
-#include <string>
 #include <errno.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
 using namespace std;
 
 inline uint32_t vsearch(vector<uint32_t> &a, vector<uint32_t> &b, uint32_t v) {
@@ -21,7 +21,7 @@ inline uint32_t vsearch(vector<uint32_t> &a, vector<uint32_t> &b, uint32_t v) {
 KSEQ_INIT(gzFile, gzread)
 int main(int argc, char *argv[])
 {
-	if (argc != 3) {
+	if (argc != 4) {
 		fprintf(stderr, "Usage: %s gi_taxid_nucl.dmp nodes.dmp nt.gz\n", argv[0]);
 		return 1;
 	}
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 	}
 	fclose(pFile);
 	end = clock();
-	fprintf(stderr,"parsing nodes.dmp into two vectors took %s seconds\n",
+	fprintf(stderr,"parsing nodes.dmp into two vectors took %f seconds\n",
 		double(end-start)/CLOCKS_PER_SEC);
 
 	start = clock();
@@ -95,51 +95,89 @@ int main(int argc, char *argv[])
 	fprintf(stderr,"1000000 tax->parent lookups took %f seconds\n",
 		double(end-start)/CLOCKS_PER_SEC);
 
-	return 0;
 	// split the nt.gz according to taxonomy
+	start = clock();
 	kseq_t *seq;
-	map<uint32_t,string> tax_path;
-	map<uint32_t,gzFile> active;
+	map<uint32_t,char*> tax_path;
+	map<uint32_t,FILE*> active;
+	map<uint32_t, uint32_t> active_used;
 	int max_open_files = 128;
 	
 	int length;
 	gzFile fp = gzopen(argv[3], "r");
 	seq = kseq_init(fp);
+	uint32_t n_seqs=0;
 	while ((length = kseq_read(seq)) >= 0) {
+		n_seqs++;
 		uint32_t gi;
-		sscanf(seq->name.s,">gi|%u|",&gi);
+		sscanf(seq->name.s,"gi|%u|",&gi);
 		uint32_t tax = vsearch(gi_vec,tax_vec,gi);
-
-		// check if this tax id is an active file
-		map<uint32_t,gzFile>::iterator it1 = active.find(tax);
-		if (it1 == active.end()) {
-			// not an active file
-			// check if we have already prepared a path
-			map<uint32_t,string>::iterator it2 = tax_path.find(tax);
-			if (it2 == tax_path.end()) {
-				// need to create the path
-				vector<uint32_t> path;
-				path.push_back(tax);
-				while (tax != 1) {
-					tax = vsearch(nodes_tax,nodes_parent,tax);
-					path.push_back(tax);
-				}
-				string path_str;
-				for(int i=path.size()-1;i>=0;i--)
-					path_str += path[i] + "/";
-				path_str += "nt.gz";
-				tax_path[tax] = path_str;
-			}
-			if (active.size() == max_open_files) {
-				// have to close a file
-				
-				// take it out of the active map
-			}
-			// open the file in append mode
+//		fprintf(stderr,"gi %u is tax %u\n",gi,tax);
+		if (tax == 0) {
+			fprintf(stderr,"failed to determine tax_id of gi %u\n",gi);
 		}
-		// write this seq to active[tax]
+		else {
+			// check if this tax id is an active file
+			map<uint32_t,FILE*>::iterator it1 = active.find(tax);
+			if (it1 == active.end()) {
+				// not an active file
+				// check if we have already prepared a path
+				map<uint32_t,char*>::iterator it2 = tax_path.find(tax);
+				if (it2 == tax_path.end()) {
+	//				fprintf(stderr,"creating path to tax\n");
+					// need to create the path
+					vector<uint32_t> path;
+					path.push_back(tax);
+					while (tax != 1) {
+						tax = vsearch(nodes_tax,nodes_parent,tax);
+	//					fprintf(stderr,",%u",tax);
+						path.push_back(tax);
+					}
+					tax = path.front();
+					char path_str[1024] = "./";
+					for(int i=path.size()-1;i>=0;i--) {
+						sprintf(path_str,"%s%u/",path_str,path[i]);
+	//					fprintf(stderr,"mkdir(%s,0755)\n",path_str);
+						mkdir(path_str,0755);
+					}
+					sprintf(path_str,"%ssplit_nt.fa",path_str);
+	//				fprintf(stderr,"file: %s\n",path_str);
+					tax_path[tax] = path_str;
+				}
+				if (active.size() == max_open_files) {
+					// have to close some files - how about the least recently used 64 files?
+					vector<uint32_t> last_used;
+					for(map<uint32_t,uint32_t>::iterator uit = active_used.begin(); uit != active_used.end(); ++uit) {
+						last_used.push_back(uit->second);
+					}
+					sort(last_used.begin(),last_used.end());
+					uint32_t mid_ru = last_used[64];
+					vector<uint32_t> toclose;
+					for(map<uint32_t,uint32_t>::iterator uit = active_used.begin(); uit != active_used.end(); ++uit)
+						if(uit->second < mid_ru)
+							toclose.push_back(uit->first);
+					for(vector<uint32_t>::iterator it3 = toclose.begin(); it3 != toclose.end(); ++it3) {
+						fclose(active[*it3]);
+						active.erase(*it3);
+						active_used.erase(*it3);
+					}
+				}
+				// open the file in append mode
+				active[tax] = fopen(tax_path[tax],"a");
+			}
+			// write this seq to active[tax]
+			fprintf(active[tax],">%s\n%s\n",seq->name.s,seq->seq.s);
+			active_used[tax] = n_seqs;
+		}
 	}
 	kseq_destroy(seq);
 	gzclose(fp);
+	// close active filehandles
+	for(map<uint32_t,FILE*>::iterator fpi = active.begin(); fpi != active.end(); ++fpi) {
+		fclose(fpi->second);
+	}
+	end = clock();
+	fprintf(stderr,"processing %u sequences took %f seconds\n", n_seqs,
+		double(end-start)/CLOCKS_PER_SEC);
 	return 0;
 }

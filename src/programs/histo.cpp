8023323c4c477a38e1eafc269aa153666122a629
Andrew Olson <olson@cshl.edu>
2013-03-25 13:07:24 -0400

    added kmerizer histogram program

---
+++ src/programs/histo.cpp
@@ -0,0 +1,34 @@
+#include <zlib.h>
+#include "kseq.h"
+#include "kmerizer.h"
+
+KSEQ_INIT(gzFile, gzread)
+int main(int argc, char *argv[])
+{
+	// parse args
+	if (argc != 7) {
+		fprintf(stderr, "Usage: %s <input file> <k> <threads> <cap_bits> <mode ('A|B|C')> <output dir>\n", argv[0]);
+		return 1;
+	}
+	gzFile fp;
+	fp = gzopen(argv[1], "r");
+	size_t k = atoi(argv[2]);
+	threads = atoi(argv[3]);
+	size_t cap_bits = atoi(argv[4]);
+	char mode = argv[5]
+	outprefix = argv[6];
+	// create output directory if it doesn't exist
+	mkdir(outprefix,0755);
+
+	kmerizer counter = new kmerizer(k, threads, outprefix, mode);
+	int rc = counter->allocate(1ULL<<cap_bits);
+
+	// process each seq from input
+	kseq_t *seq = kseq_init(fp);
+	int length;
+	while ((length = kseq_read(seq)) >= 0)
+		if ((size_t)length >= k)
+			rc = counter->addSequence(seq->seq.s,length);
+	rc = counter->histogram();
+	return 0;
+}

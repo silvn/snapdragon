fa81d597bd4107e588f299d1edca7d92a96bdf12
Andrew Olson <olson@cshl.edu>
2013-05-29 02:00:42 -0400

    bug fixes, memory leaks, timing functions

--- src/programs/kcount.cpp
+++ src/programs/kcount.cpp
@@ -8,31 +8,30 @@ int main(int argc, char *argv[])
 {
 	// parse args
 	if (argc != 7) {
-		fprintf(stderr, "Usage: %s <input file> <k> <threads> <cap_bits> <mode ('A|B|C')> <output dir>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <input file> <k> <threads> <cap_bytes> <mode ('A|B|C')> <output dir>\n", argv[0]);
 		return 1;
 	}
 	gzFile fp;
 	fp = gzopen(argv[1], "r");
 	size_t k = atoi(argv[2]);
 	size_t threads = atoi(argv[3]);
-	size_t cap_bits = atoi(argv[4]);
+	size_t cap_bytes = atoi(argv[4]);
 	char* mode = argv[5];
 	char* outprefix = argv[6];
 	// create output directory if it doesn't exist
 	mkdir(outprefix,0755);
 
 	Kmerizer *counter = new Kmerizer(k, threads, outprefix, mode[0]);
-	int rc = counter->allocate(1ULL<<cap_bits);
+	int rc = counter->allocate(3000000000);
 	if (rc != 0) {
-		fprintf(stderr,"failed to allocate %llu bytes\n",1ULL<<cap_bits);
+		fprintf(stderr,"failed to allocate %llu bytes\n",cap_bytes);
 		exit(1);
 	}
 	// process each seq from input
 	kseq_t *seq = kseq_init(fp);
 	int length;
 	while ((length = kseq_read(seq)) >= 0)
-		if ((size_t)length >= k)
-			counter->addSequence(seq->seq.s,length);
+        counter->addSequence(seq->seq.s,length);
 	kseq_destroy(seq);
 	gzclose(fp);
 	counter->save();
